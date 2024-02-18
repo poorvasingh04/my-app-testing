98ee16cfec678505758caecde5bf9e5f2f1a802f
Poorva Singh <poorvasingh2471@gmail.com>
2024-02-18 10:12:36 +0100

    first deploy in heroku

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { useEffect } from 'react';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC9WcbEUlG-f2wmmMNoeaSfSzZFJN3DdIM",
+  authDomain: "my-app-e6ecd.firebaseapp.com",
+  projectId: "my-app-e6ecd",
+  storageBucket: "my-app-e6ecd.appspot.com",
+  messagingSenderId: "254101470394",
+  appId: "1:254101470394:web:e58ff669b5f998a00c9683"
+};
+initializeApp(firebaseConfig);
 
 function App() {
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my project my-app. Hello
         </p>
         <a
           className="App-link"
