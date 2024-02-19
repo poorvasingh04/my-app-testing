86c71815f81da339643010379bba60db7425188b
Poorva Singh <poorvasingh2471@gmail.com>
2024-02-19 09:38:10 +0100

    feat: remove api keys

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import { initializeApp } from "firebase/app";
 import { useEffect } from 'react';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyC9WcbEUlG-f2wmmMNoeaSfSzZFJN3DdIM",
-  authDomain: "my-app-e6ecd.firebaseapp.com",
-  projectId: "my-app-e6ecd",
-  storageBucket: "my-app-e6ecd.appspot.com",
-  messagingSenderId: "254101470394",
-  appId: "1:254101470394:web:e58ff669b5f998a00c9683"
-};
-initializeApp(firebaseConfig);
-
 function App() {
   useEffect(() => {
+    // Initialize Firebase
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+      appId: process.env.REACT_APP_FIREBASE_APPID
+    };
 
+    initializeApp(firebaseConfig);
   }, []);
 
   return (
